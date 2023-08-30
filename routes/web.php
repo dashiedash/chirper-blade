d6b3f98311482028c822ea21ec1cd298d35d0b27
Faith Flores <31542077+dashiedash@users.noreply.github.com>
2023-08-30 11:30:30 +0800

    Implement edit/update chirp routes

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,7 @@ Route::middleware('auth')
     });
 
 Route::resource('chirps', ChirpController::class)
-    ->only(['index', 'store'])
+    ->only(['index', 'store', 'edit', 'update'])
     ->middleware(['auth', 'verified']);
 
 require __DIR__ . '/auth.php';
